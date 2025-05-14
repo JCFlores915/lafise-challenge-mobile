086a841366eb4fa940085343822136bddc4b7d0c
JCFlores915 <19163047+JCFlores915@users.noreply.github.com>
2025-05-14 16:14:48 -0600

    feat: actualizar URL de API en Constants.ts para pruebas locales

--- constants/Constants.ts
+++ constants/Constants.ts
@@ -1,4 +1,4 @@
 
 // el ip de tu computadora local para poder hacer pruebas localmente
-export const API_URL = 'http://172.25.144.1:5566';
+export const API_URL = 'http://TU_IP_ORDENADOR:5566';
 export const TIMEOUT = 10000; 
